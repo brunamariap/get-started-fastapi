821af5a89c00f239aa394541f1c09cd4e29064d1
Bruna Maria <bruna.mariap@outlook.com>
2023-10-03 17:41:25 -0300

    Adjusts

--- app/main.py
+++ app/main.py
@@ -9,13 +9,6 @@ import asyncio
 
 from app.background import create_item_log
 
-""" 
-Middleware para reportar toda e qualquer tentativa de acessar os endpoints (registrar o máximo de dados possíveis)
-Assincronismo, ter alguma endpoint que seja assíncrono, praticando o conceito
-Criar uma tarefa de background, pode ser somente o log, como visto em sala de aula
-Utilizar o padrão de inversão de controle por meio de injeção de dependência 
-"""
-
 app = FastAPI()
 
 items = dict()
@@ -54,38 +47,32 @@ async def log_requests(request: Request, call_next):
     response = await call_next(request)
     process_time = datetime.now() - start_time
     response.headers["X-Time-Elapsed"] = str(process_time)
-    # print(response.headers)
     return response
 
 
 @app.get("/items")
 def list_items():
-    return items
+    return JSONResponse(content=jsonable_encoder(items), status_code=status.HTTP_200_OK)
 
 
 @app.post("/items/create")
 def create_item(bg_task: BackgroundTasks, item : Item):
     if item.name is None and item.price is None:
-        return {"message": "invalid item"}
-    elif not items.get(item.name) is None:
-        return {"message": "item exists"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Body is required")
     else:
         item_dict = jsonable_encoder(item)
-        # del item_dict['id']
         item_temp = ItemDependecy(**item_dict)
         items[item_temp.id] = item_temp
         bg_task.add_task(create_item_log, item_name=item_temp.name, item_price=item_temp.price)
-        return JSONResponse(content=jsonable_encoder(item), status_code=status.HTTP_201_CREATED)
+        
+        return JSONResponse(content=jsonable_encoder(item_temp), status_code=status.HTTP_201_CREATED)
 
 
 @app.put("/items/update/{item_id}")
 def update_item(item_id: UUID, item = Depends(ItemDependecy)):
     if item_id is None or item_id not in items:
-        return {"message": "invalid item"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid item")
     else:
-        if item is None:
-            return {"message": "price is required"}
-
         items[item_id].name = item.name
         items[item_id].price = item.price 
         return JSONResponse({"messagem": "updated item"}, status_code=status.HTTP_200_OK)
@@ -95,7 +82,6 @@ def update_item(item_id: UUID, item = Depends(ItemDependecy)):
 def delete_item(item_id: UUID):
     if item_id not in items or item_id is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
-        # return {"message": "invalid item"}
     else:  
         del items[item_id]
         return JSONResponse(content={}, status_code=status.HTTP_204_NO_CONTENT)
@@ -110,7 +96,6 @@ async def list_specific_item(item_id: UUID):
         return JSONResponse(content=jsonable_encoder(items[item_id]), status_code=status.HTTP_200_OK)
 
 
-# Para criar alguns itens sem que seja preciso adicionar de 1 por 1
 @app.post("/items/import")
 def import_items():
     with open("items.json", 'r', encoding='utf-8') as items_import:
